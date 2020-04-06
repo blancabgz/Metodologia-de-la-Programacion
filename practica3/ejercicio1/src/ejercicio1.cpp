aaa9ced6239e15a2eba603000802ee9bcb430876
blancabgz <blancabril.999@gmail.com>
2020-04-06 14:09:43 +0200

    Ejercicio 1 terminado para revisar

---
+++ practica3/ejercicio1/src/ejercicio1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "utilidades.h"
+using namespace std;
+
+int main(){
+  int tam;
+  cout << "Indica el numero de elementos que va a introducir: ";
+  cin >> tam;
+  //Vigila que no introduzca el valor 0
+  if(tam == 0){
+    cout << "No se puede introducir este valor" << endl;
+    return 0;
+  }
+
+  double vector[tam];
+  introducirValores(vector,tam);
+  double *vectorPunteros[tam];
+  apuntarVector(vector,vectorPunteros,tam);
+  ordenarVectorPunteros(vectorPunteros,tam);
+  mostrarDatosVector(vector,tam,vectorPunteros);
+
+
+}
---
+++ practica3/ejercicio1/src/utilidades.cpp
@@ -0,0 +1,47 @@
+#include "utilidades.h"
+#include <iostream>
+using namespace std;
+
+void introducirValores(double array[], int tam){
+  double valor;
+  for(int i=0;i<tam;i++){
+    if(i < tam-1){
+      cout << "Introduce un elemento: " ;
+    }else{
+      cout << "Introduce el último elemento:" ;
+    }
+    cin >> valor;
+    array[i] =  valor;
+  }
+}
+
+void apuntarVector(double *vector, double **ptr_vector, int tam){
+  for(int i=0; i<tam; i++){
+    ptr_vector[i] = &vector[i];
+  }
+}
+
+void ordenarVectorPunteros(double **ptr_vector, int tam){
+  double *temporal;
+  for(int i=0; i<tam; i++){
+    for(int j=0; j<tam-1; j++){
+      if(*ptr_vector[j] > *ptr_vector[j+1]){
+        temporal = ptr_vector[j];
+        ptr_vector[j] = ptr_vector[j+1];
+        ptr_vector[j+1] = temporal;
+      }
+    }
+  }
+}
+
+void mostrarDatosVector(double array[], int tam, double **ptr_vector){
+  for(int i=0; i<tam; i++){
+    cout << array[i] << " ";
+  }
+  cout << endl;
+  
+  for(int j=0; j<tam; j++){
+    cout << **(ptr_vector+j)<< " ";
+  }
+  cout << endl;
+}
